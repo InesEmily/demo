a25f29cdf3b2bd900426b7fbee3691751194f5e2
ines <r0306129@student.thomasmore.be>
2022-10-25 12:26:02 +0200

    alles aangepast

--- src/main/java/be/thomasmore/demo/HomeController.java
+++ src/main/java/be/thomasmore/demo/HomeController.java
@@ -1,13 +1,21 @@
 package be.thomasmore.demo;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-  @GetMapping("/")
-    public String home(){
-      return "homepage";
+    @Value("${specialNumber:111}")
+    private int specialNumber;
 
-  }
+    @GetMapping("/")
+    public String homepage(Model model) {
+        model.addAttribute("specialNumber", specialNumber);
+        return "homepage";
+
+
+    }
 }
